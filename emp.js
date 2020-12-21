624c9a1b4234dc935b00b98345d68836c92211e6
“teji1998” <tejasvinirpk@gmail.com>
2020-12-22 00:42:18 +0530

    Added:Code to check if employee is present or not

--- emp.js
+++ emp.js
@@ -1,2 +1,12 @@
 console.log("Welcome to employee wage problem using javascript ");
 
+//UC1:To check if employee is present or not
+{
+    const IS_ABSENT = 0;
+    let empCheck = Math.floor(Math.random()*10)%2;
+    if(empCheck == IS_ABSENT){
+        console.log("Employee is absent");
+    }else{
+        console.log("Employee is present")
+    }
+}
